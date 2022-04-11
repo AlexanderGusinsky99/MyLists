c00c4b97d94f0bebce714f119a09d07c5cdb5cf0
Alexander Gusinsky <alexander.gusinsky99@yandex.ru>
2022-04-11 18:32:52 +0300

    Add Methods
    added methods: equal, to string, delete by index

--- Lists/LinkedList.cs
+++ Lists/LinkedList.cs
@@ -8,32 +8,99 @@ namespace Lists
 {
     public class LinkedList : IList
     {
-        public int Length { get; private set; }
         private Node _root;
         private Node _tail;
+        public int this[int index]
+        {
+            get
+            {
+
+                Node crnt = _root;
+                for (int i = 1; i <= index; i++)
+                {
+                    crnt = crnt.Next;
+                }
+                return crnt.Value;
+            }
+            set
+            {
+            }
+        }
+        public int Length
+        {
+            get
+            {
+                int count = 0;
+                Node crnt = _root;
+                while (crnt.Next != null)
+                {
+                    crnt = crnt.Next;
+                    count++;
+                }
+                return count;
+            }
+            private set
+            {
+            }
+        }
         public LinkedList()
         {
-            Length = 0;
             _root = null;
             _tail = null;
         }
         public LinkedList(int value)
         {
-            Length = 1;
             _root = new Node(value);
             _tail = _root;
         }
         public void Add(int value)
         {
-            if (_root == null)
+            _tail = new Node(value);
+            _tail = _tail.Next;
+        }
+        public void DeleteByIndex(int index)
+        {
+            Node crnt = _root;
+            for (int i = 1; i < index-1; i++)
             {
-                _root = new Node(value);
+                crnt = crnt.Next;
             }
-            else
+            crnt.Next = crnt.Next.Next;
+        }
+        public override string ToString()
+        {
+            string s = "";
+            Node crnt = _root;
+            while (crnt != null)
             {
-                _tail.Next = new Node(value);
-                _tail = _tail.Next;
+                s += $"{crnt.Value} ";
+                crnt = crnt.Next;
             }
-         }
+            return s;
+        }
+        public override bool Equals(object? obj)
+        {
+            if (obj == null || !(obj is LinkedList))
+            {
+                return false;
+            }
+            LinkedList list = (LinkedList)obj;
+            if (list.Length != this.Length)
+            {
+                return false;
+            }
+            Node thisCrnt = this._root;
+            Node listCrnt = list._root;
+            while(thisCrnt != null)
+            {
+                if (thisCrnt.Value != listCrnt.Value)
+                {
+                    return false;
+                }
+                thisCrnt = thisCrnt.Next;
+                listCrnt = listCrnt.Next;
+            }
+            return true;
+        }
     }
 }
